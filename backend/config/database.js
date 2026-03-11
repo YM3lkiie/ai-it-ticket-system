35de1b503f5da86a4142fcb273d454538eb7f343
Melkiie <youlcindamelk@gmail.com>
2026-03-11 09:40:07 +0200

    Updated server code to get and access database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,20 @@
 // DATABASE CONNECTION FILE
+const { connect } = require("mssql");
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "Mysql@123",
-    databse
-})
+    databse: "ai_ticket_system"
+});
+
+connection.connect((error) => {
+    if(error) {
+        console.error("Database connection failed:", error);
+        returen;
+    }
+    console.log("Conencted to MySQL Database");
+});
+
+module.exports = connection
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+/*
 const express = require("express");
 const cors = require("cors");
 
@@ -12,6 +13,28 @@ app.get("/", (req, res) => {
 
 const PORT = 3000;
 
+app.listen(PORT,() => {
+    console.log(`Sever running on ${PORT}`);
+});
+*/
+
+const express = require("express");
+const db = require("./config/database");
+
+const app = express();
+app.use(express.json());
+
+app.get("/test-db", (req, res) => {
+    db.query("SELECT 1", (error, results) => {
+        if(error) {
+            res.status(500).send("Database error");
+            return;
+        }
+        res.send("Database connected successfully");
+    })
+});
+
+const PORT = 3000;
 app.listen(PORT,() => {
     console.log(`Sever running on ${PORT}`);
 });
